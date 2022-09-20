b1663a90617696412b1d7f5ea567bddbc5bd17ff
spolisar <spolisar@gmail.com>
2022-09-20 14:37:30 -0400

    improved grep.rb

--- week1-ruby/grep.rb
+++ week1-ruby/grep.rb
@@ -1,6 +1,7 @@
 filename = ARGV[0]
 phrase = ARGV[1]
 
-File.open(filename, 'r').each do |line|
-    puts line if line.match(/#{phrase}/)
+File.open(filename, 'r') do |f|
+    f.each {|line|puts "#{f.lineno}: #{line}" if line.match(/#{phrase}/)}
+    # puts "#{#{line}" if line.match(/#{phrase}/)
 end
